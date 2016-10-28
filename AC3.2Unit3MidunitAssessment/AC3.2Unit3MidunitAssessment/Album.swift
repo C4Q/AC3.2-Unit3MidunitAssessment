40a17980e2c413a6fe3fed0cf9238e17c4ae321c
Mira Estil <imz.estil@gmail.com>
2016-10-28 10:51:28 -0400

    updates

---
+++ AC3.2Unit3MidunitAssessment/AC3.2Unit3MidunitAssessment/Album.swift
@@ -0,0 +1,13 @@
+//
+//  Spotify.swift
+//  AC3.2Unit3MidunitAssessment
+//
+//  Created by Ilmira Estil on 10/28/16.
+//  Copyright © 2016 C4Q. All rights reserved.
+//
+
+import Foundation
+
+internal struct Album {
+
+}
